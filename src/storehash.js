ab7ac81b06584c1f0d3e55d85f4f3c67360b5f97
kvutien <kvutien@VTKT5.fritz.box>
2021-05-16 12:50:37 +0200

    initialise dApp from React boilerplate

--- src/storehash.js
+++ src/storehash.js
@@ -2,7 +2,7 @@ import getWeb3 from "./getWeb3";
 // export an instance of the smart contract that stores IPFS hash
 
 async function storehash() { 
-    // Your contract address
+    // Your contract address as deployed by Remix
     const address = '0x40619f0278DE88aB843d8c57A6f6c66e3ab49dd1';   // address deployed in Remix
     // Your contract ABI as copied from Remix
     const abi = [{
